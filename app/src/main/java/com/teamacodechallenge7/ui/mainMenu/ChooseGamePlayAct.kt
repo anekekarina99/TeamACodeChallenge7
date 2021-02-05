1d0340a97e476c92c08dae784da3c7dbdf571f41
reinaldoriant <reinaldoriant@gmail.com>
2021-02-05 17:14:16 +0700

    memperbaiki ui choose game play kt

--- app/src/main/java/com/teamacodechallenge7/ui/mainMenu/ChooseGamePlayAct.kt
+++ app/src/main/java/com/teamacodechallenge7/ui/mainMenu/ChooseGamePlayAct.kt
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil
 import com.teamacodechallenge7.R
 import com.teamacodechallenge7.databinding.ActivityChooseGamePlayBinding
 import com.teamacodechallenge7.databinding.ActivityLoginBinding
+import com.teamacodechallenge7.ui.playgamevscomputer.PlayGameVsComputer
 import com.teamacodechallenge7.ui.playgamevsplayer.PlayGameVsPlayer
 import com.teamacodechallenge7.ui.signUp.SignUpActivity
 
@@ -20,9 +21,11 @@ class ChooseGamePlayAct : AppCompatActivity() {
             )
         binding.btPlayOther.setOnClickListener {
             startActivity(Intent(this, PlayGameVsPlayer::class.java))
+            finish()
         }
         binding.btPlayComputer.setOnClickListener {
-
+            startActivity(Intent(this, PlayGameVsComputer::class.java))
+            finish()
         }
         binding.btBackMenu.setOnClickListener {
             startActivity(Intent(this, MainMenuAct::class.java))
