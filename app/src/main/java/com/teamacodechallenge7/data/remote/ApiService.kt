e947ae4ce910d73bb2106bdc4331e3aa5d20ec43
reinaldoriant <reinaldoriant@gmail.com>
2021-02-05 09:30:20 +0700

    refresh token

--- app/src/main/java/com/teamacodechallenge7/data/remote/ApiService.kt
+++ app/src/main/java/com/teamacodechallenge7/data/remote/ApiService.kt
@@ -6,6 +6,7 @@ import com.teamacodechallenge7.data.model.GetBattle
 import com.teamacodechallenge7.data.model.Users
 import com.teamacodechallenge7.data.model.LoginMsg
 import com.teamacodechallenge7.data.model.LoginRequest
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
 import okhttp3.MultipartBody
@@ -13,25 +14,17 @@ import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface ApiService {
-//    @GET("/api/posts")
-//    fun getPost(): Single<PostMsg>
-//
+
     @POST("/api/v1/auth/login")
     fun loginAction(@Body requestLogin: LoginRequest): Single<LoginMsg>
 
     
     @POST("/api/v1/auth/register")
     fun regis(@Body requestRegister: SignUpRequest): Single<SignUpMsg>
-//
-//    @GET("/api/v1/auth/me")
-//    fun me(@Header("Authorization") authorization: String): Single<MeMsg>
-//
-//    @PUT("/api/v1/users")
-//    fun me(@Header("Authorization") authorization: String): Single<MeMsg>
 
     @GET("/api/v1/battle")
     fun getBattle(
-        @Header("Authorization") authorization: String): Observable<List<GetBattle>>
+        @Header("Authorization") authorization: String): Flowable<GetBattle>
 
     @GET("/api/v1/users")
     fun getUsers(
--- app/src/main/java/com/teamacodechallenge7/ui/skor/SkorViewModel.kt
+++ app/src/main/java/com/teamacodechallenge7/ui/skor/SkorViewModel.kt
@@ -24,20 +24,18 @@ class SkorViewModel(
     val tag = "Skor"
     private var disposable: Disposable? = null
 //    val token = pref.token
-    val token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDE1N2I0OGRiNzg0ODAwMTczZjk4YjkiLCJ1c2VybmFtZSI6ImFndW5ndyIsImVtYWlsIjoiYWd1bmd3QHlhaG9vLmNvbSIsImlhdCI6MTYxMjM1NzQxMCwiZXhwIjoxNjEyMzY0NjEwfQ.Knn2frT7Wnvldl6iNmSEI5ec3yJlO5N1h2cU-MvDdC8"
+    val token = SharedPref.token.toString()
 
     fun listSkor(recyclerView: RecyclerView, context: Context) {
-        recyclerView.layoutManager = LinearLayoutManager(
-            context, LinearLayoutManager.VERTICAL, false
-        )
+
         disposable = service.getBattle(pref.token.toString())
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
 //                resultMe = it
-                val adapter = SkorAdapter(it, context)
+               /* val adapter = SkorAdapter(,context)
                 adapter.notifyDataSetChanged()
-                recyclerView.adapter = adapter
+                recyclerView.adapter = adapter*/
             }, {
                 it.printStackTrace()
             })
